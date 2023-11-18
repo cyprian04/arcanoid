3daddbc715b3cbc22ee08e7dd0e777cac99c55e9
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-18 17:48:06 +0100

    optimized the statements for paddle move

--- _Paddle.py
+++ _Paddle.py
@@ -7,21 +7,14 @@ class Paddle():
         self.pos_y = pos_y
         self.width = width
         self.height = height
-        self.paddle_color = (0,255,0)
+        self.paddle_color = (0,200,0)
         self.draw()
 
     def draw(self):
         pygame.draw.rect(self.screen, self.paddle_color, (self.pos_x, self.pos_y, self.width, self.height))
 
     def move(self, x_pos):
-        if self.pos_x + x_pos <= 0:
-            self.pos_x = 0;
-
-        elif self.pos_x + self.width + x_pos >= 700:
-            self.pos_x = 700- self.width;
-
-        elif self.pos_x + x_pos > 0 and x_pos < 0:
-            self.pos_x += x_pos  
-
-        elif self.pos_x + self.width + x_pos < 700 and x_pos > 0:
-            self.pos_x += x_pos 
+        if x_pos < 0:
+            self.pos_x = max(0, self.pos_x + x_pos)
+        elif x_pos > 0:
+            self.pos_x = min(700 - self.width, self.pos_x + x_pos)
