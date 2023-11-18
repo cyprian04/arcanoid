ce335b6681dba7a41c28510269b0fcf14c7deb8b
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-18 16:35:40 +0100

    Changed the Paddle class + added move and draw func's and statements for them in gameloop

--- _Paddle.py
+++ _Paddle.py
@@ -1,10 +1,17 @@
 import pygame
 
-class Paddle(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__()
-        self.image = pygame.Surface((100, 10))
-        self.image.fill((0, 255, 0))
-        self.rect = self.image.get_rect()
-        self.rect.x = pos_x
-        self.rect.y = pos_y
+class Paddle():
+    def __init__(self, screen, pos_x, pos_y, width, height):
+        self.screen = screen
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.width = width
+        self.height = height
+        self.paddle_color = (0,255,0)
+        self.draw()
+
+    def draw(self):
+        pygame.draw.rect(self.screen, self.paddle_color, (self.pos_x, self.pos_y, self.width, self.height))
+
+    def move(self, x_pos):
+        self.pos_x += x_pos     
--- arcanoid.py
+++ arcanoid.py
@@ -7,20 +7,27 @@ width, height = 700, 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Cyprian's Arkanoid")
 
-sprites = pygame.sprite.Group()
-paddle = Paddle(300,450)
-sprites.add(paddle)
-screen.fill((0,0,0))
+paddle = Paddle(screen,300,450, 40, 10)
+paddle_speed = 5
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    
             sys.exit()
 
-    pygame.display.flip()
-    sprites.draw(screen)
+    screen.fill((0,0,0))
+    keys = pygame.key.get_pressed() 
+    paddle.draw()
+
+    if keys[pygame.K_RIGHT]:
+        paddle.move(5)
+    elif keys[pygame.K_LEFT]:
+        paddle.move(-5)
+
+    pygame.display.update()
+    clock.tick(60)
     
 
     
