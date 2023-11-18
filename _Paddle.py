93805a85a69ee09b2e56bd0a2e65979adb35637a
Cyprian <Cyprian.Gibas@o2.pl>
2023-11-18 15:12:58 +0100

    added Paddle class + configurated window and paddle to screen

--- _Paddle.py
+++ _Paddle.py
@@ -1,1 +1,8 @@
 import pygame
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((100, 10))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect()
--- arcanoid.py
+++ arcanoid.py
@@ -1,16 +1,25 @@
 import pygame
 import sys
+from _Paddle import Paddle
 
 pygame.init()
-
 width, height = 700, 500
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Cyprian's Arkanoid")
+
+sprites = pygame.sprite.Group()
+paddle = Paddle()
+sprites.add(paddle)
+screen.fill((0,0,0))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    pygame.display.flip()
+    sprites.draw(screen)
+    
 
     
